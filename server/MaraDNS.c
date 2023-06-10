0c946f8de03e0411265d10c74bcabd6affd66a9e
Tony Solomonik <tony.solomonik@gmail.com>
2023-06-10 20:37:27 +0300

    Persistent mode

--- server/MaraDNS.c
+++ server/MaraDNS.c
@@ -4682,7 +4682,7 @@ int cmain(int argc, char **argv) {
 
 #include "stdio.h"
 
-int main(int argc, char **argv) {
+void run(int argc, char **argv, unsigned char *buf, int len) {
   js_string *incoming = 0;
   js_string *uncomp = 0;
   conn ect;
@@ -4711,8 +4711,49 @@ int main(int argc, char **argv) {
       harderror(L_UCL); /* "Could not set locale for uncomp string" */
   }
 
-  read(STDIN_FILENO, incoming->string, 512);
+  memcpy(incoming->string, buf, len);
 
   decompress_data(incoming, uncomp);
   proc_query(uncomp, &ect, STDOUT_FILENO);
+
+  js_destroy(incoming);
+  js_destroy(uncomp);
+  js_dealloc(ipv4_client);
+}
+
+/* this lets the source compile without afl-clang-fast/lto */
+#ifndef __AFL_FUZZ_TESTCASE_LEN
+
+ssize_t       fuzz_len;
+unsigned char fuzz_buf[1024000];
+
+  #define __AFL_FUZZ_TESTCASE_LEN fuzz_len
+  #define __AFL_FUZZ_TESTCASE_BUF fuzz_buf
+  #define __AFL_FUZZ_INIT() void sync(void);
+  #define __AFL_LOOP(x) \
+    ((fuzz_len = read(0, fuzz_buf, sizeof(fuzz_buf))) > 0 ? 1 : 0)
+  #define __AFL_INIT() sync()
+
+#endif
+
+__AFL_FUZZ_INIT();
+
+#pragma clang optimize off
+#pragma GCC optimize("O0")
+
+int main(int argc, char **argv) {
+#ifdef __AFL_HAVE_MANUAL_CONTROL
+  __AFL_INIT();
+#endif
+
+  unsigned char *buf = __AFL_FUZZ_TESTCASE_BUF;
+
+  while (__AFL_LOOP(10000)) {
+    int len = __AFL_FUZZ_TESTCASE_LEN;
+    if (len > 512) continue;
+
+    run(argc, argv, buf, len);
+  }
+
+  return 0;
 }
